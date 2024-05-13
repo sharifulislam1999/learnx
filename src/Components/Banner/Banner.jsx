4ce6e26cca15d7f6bae4100e38a29aacd005fc6f
Shariful Islam <shariful.feb1999@gmail.com>
2024-05-13 23:20:39 +0600

    add home animation

--- src/Components/Banner/Banner.jsx
+++ src/Components/Banner/Banner.jsx
@@ -4,6 +4,9 @@ import user2 from "../../assets/Images/User/user2.png"
 import user3 from "../../assets/Images/User/user3.jpg"
 import user4 from "../../assets/Images/User/user1.jpg"
 import { Link } from 'react-router-dom';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+AOS.init();
 const Banner = () => {
     return (
         <div className="bgimage py-20 lg:py-40 bg-cover bg-center">
@@ -11,12 +14,12 @@ const Banner = () => {
             <div className="flex gap-2 flex-col lg:flex-row justify-between">
                 <div className="flex-1">
                     <div className="mb-2 md:mb-6">
-                    <h1 className="text-3xl md:text-6xl leading-snug font-bold text-white">Unlock Knowledge in <br/><span className="text-[#FAB519]">learn.<span className="text-white">X</span></span></h1>
+                    <h1 className="text-3xl md:text-6xl leading-snug font-bold text-white" data-aos="fade-up" data-aos-duration="700"   data-aos-easing="ease-in-out">Unlock Knowledge in <br/><span className="text-[#FAB519]">learn.<span className="text-white">X</span></span></h1>
                     </div>
-                    <div className="text-[#d3d1d1e3] text-sm leading-6 font-medium">
+                    <div className="text-[#d3d1d1e3] text-sm leading-6 font-medium" data-aos="fade-up" data-aos-duration="700" data-aos-delay="600"   data-aos-easing="ease-in-out">
                         <p>Join forces with fellow learners on our interactive group study platform. Enhance your understanding, share insights, and conquer challenges together. Experience the power of collective knowledge as you collaborate, discuss, and achieve academic excellence. Start studying smarter, not harder, with our group study website today!</p>
                     </div>
-                    <div className="flex items-center mt-6">
+                    <div className="flex items-center mt-6" data-aos="fade-up" data-aos-duration="700" data-aos-delay="1000"   data-aos-easing="ease-in-out">
                         <div className="h-12 w-12 border-2 border-[#FAB519] rounded-full">
                         <img src={user1} className="w-full h-full rounded-full" alt="" />    
                         </div>                        
@@ -34,7 +37,7 @@ const Banner = () => {
 
                         </div>             
                     </div>
-                    <div className="mt-8">
+                    <div className="mt-8" data-aos="fade-up" data-aos-duration="700" data-aos-delay="1100"   data-aos-easing="ease-in-out">
                         <Link className="px-6 py-2 rounded-lg text-lg font-semibold border border-[#FAB519] duration-200 hover:bg-transparent hover:text-white  bg-[#FAB519]" to='/assignments'>All Assingment</Link>
                         </div> 
                 </div>
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,9 +1,11 @@
 import { useLoaderData } from 'react-router-dom';
 import Banner from './../../Components/Banner/Banner';
-import AssignmentCard from '../../Components/AssignmentCard/AssignmentCard';
 import Title from '../../Components/Title/Title';
 import Accordian from '../../Components/Accordian/Accordian';
 import Featurecard from '../../Components/Featurecard/Featurecard';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+AOS.init();
 const Home = () => {
     const assignment = useLoaderData()
     document.title = "Learn.X | Home"
@@ -11,16 +13,16 @@ const Home = () => {
        <>
         <Banner></Banner>
         <div className="container mx-auto px-3">
-            <div>
+            <div data-aos="fade-up">
                 <Title title="Explore Our Assignments" des="Delve into a comprehensive overview of assignment-focused functionalities on our group study website, empowering users to leverage these tools for streamlined collaborative learning."></Title>
             </div>
-        <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6'>
+        <div data-aos="fade-up" className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6'>
             {assignment.map((item,i)=> <Featurecard key={i} item={item}></Featurecard>)}
         </div>
-            <div className='mt-40'>
+            <div className='mt-40' data-aos="fade-up">
                 <Title title="What Say Our Client" des="Delve into a comprehensive overview of assignment-focused functionalities on our group study website, empowering users to leverage these tools for streamlined collaborative learning."></Title>
             </div>
-            <div>
+            <div data-aos="fade-up" data-aos-delay="600">
                 <Accordian></Accordian>
             </div>
         </div>
